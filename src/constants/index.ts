4120b0288ed5f3df90fecc9511cc2dc2e2ac8a22
Duong TRINH <khanhduong.dav@gmail.com>
2024-10-18 12:50:03 +0200

    refactor: move route paths to constants file

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,8 @@
 export const DELAY : number = 250;
 export const LENGTH_OF_DESCRIPTION: number = 100;
 export const NUMBER_RECIPE_PER_PAGE: number = 9;
+
+export const PATH = {
+  "HOME" : "/",
+  "ERROR": "error"
+}
